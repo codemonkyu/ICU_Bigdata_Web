6cd2b8bd585bd51cd7fa335757f9689c3d90b815
codemonkyu <codemonkyu@gmail.com>
2022-06-24 12:30:54 +0900

    fix: 1.1

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("http://3.37.32.74/")
 def index():
    return render_template('safe_rank.html')
 
-@app.route("/cctv")
+@app.route("http://3.37.32.74/cctv")
 def cctv():
    return render_template('cctv.html')
 
-@app.route("/police")
+@app.route("http://3.37.32.74/police")
 def police():
    return render_template('police.html')
 
-@app.route("/nocover")
+@app.route("http://3.37.32.74/nocover")
 def nocover():
    return render_template('nocover.html')
 
-@app.route("/localhouse")
+@app.route("http://3.37.32.74/localhouse")
 def localhouse():
    return render_template('local+house.html')
 
