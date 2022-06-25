ca687ed7ed5cd96639afc7ec81a668c4a4a69e27
codemonkyu <codemonkyu@gmail.com>
2022-06-25 10:45:39 +0900

    add: 6/26

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route("http://3.37.32.74/")
+@app.route("/")
 def index():
    return render_template('safe_rank.html')
 
-@app.route("http://3.37.32.74/cctv")
+@app.route("/cctv")
 def cctv():
    return render_template('cctv.html')
 
-@app.route("http://3.37.32.74/police")
+@app.route("/police")
 def police():
    return render_template('police.html')
 
-@app.route("http://3.37.32.74/nocover")
+@app.route("/nocover")
 def nocover():
    return render_template('nocover.html')
 
-@app.route("http://3.37.32.74/localhouse")
+@app.route("/localhouse")
 def localhouse():
    return render_template('local+house.html')
 
